ffc6998b8bb2bfdfcd3fd19dda2214c671154b7f
piyushrai <thepiyushrai@gmail.com>
2023-08-22 08:56:09 +0530

    code optimised

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import TodoRowItem from './comoponents/TodoRowItem';
 import TodoTable from './comoponents/TodoTable';
+import NewTodoForm from './comoponents/NewTodoForm';
 import React, {useState} from 'react';
 /**
  * @author Piyush Rai
@@ -9,20 +10,49 @@ import React, {useState} from 'react';
 
  */
 function App() {
+  const [showAddTodoForm, setShowAddTodoForm] = useState(false);
   const [todos, setTodos] = useState([
-    {rowNumber: 1, rowDescription: 'class work ', rowAssigned: 'User no One'},
-    {rowNumber: 2, rowDescription: ' planting', rowAssigned: 'User no Two'},
-    {rowNumber: 3, rowDescription: ' Maths', rowAssigned: 'User no One'},
-    {rowNumber: 4, rowDescription: 'Hindi study', rowAssigned: 'User no One'}
+    {rowNumber: 1, rowDescription: 'Class work ', rowAssigned: 'User no One'},
+    {rowNumber: 2, rowDescription: ' Planting', rowAssigned: 'User no Two'},
+    {rowNumber: 3, rowDescription: ' Maths', rowAssigned: 'User no three'},
+    {rowNumber: 4, rowDescription: 'Hindi study', rowAssigned: 'User no four'}
   ]
   )
+  const addTodo = (description, assigned) => {
+    let rowNumber = 0;
+    if (todos.length > 0) {
+      rowNumber = todos[todos.length - 1].rowNumber + 1;
+    } else {
+      rowNumber = 1;
+    }
+      const newTodo = {
+        rowNumber: rowNumber,
+        rowDescription: description,
+        rowAssigned: assigned
+      };
+      setTodos(todos => [...todos, newTodo])
+  }
+
+  const deleteTodo = (deleteTodoRowNumber) => {
+    let filtered = todos.filter(function (value) {
+      return value.rowNumber !== deleteTodoRowNumber;
+    });
+    setTodos(filtered);
+  }
   return (
    <div className='mt-5 container'>
       <div className="card">
       <body>
       <div className="card-header"><h2>Your TODO </h2></div>
 <div className='card-body'>
-   <TodoTable todos = {todos}></TodoTable>
+   
+   <TodoTable todos={todos} deleteTodo={deleteTodo}/>
+          <button onClick={() => setShowAddTodoForm(!showAddTodoForm)} className='btn btn-primary'>
+            {showAddTodoForm ? 'Close New Todo' : 'New Todo'}
+          </button>
+        {showAddTodoForm && 
+          <NewTodoForm addTodo={addTodo}/>
+        }
     </div>
 </body>
 </div>
--- src/comoponents/TodoTable.js
+++ src/comoponents/TodoTable.js
@@ -1,14 +1,13 @@
 import TodoRowItem from "./TodoRowItem"
+/**
+ * @author Piyush Rai
+ * @date 22.08.2023
+
+ */
 function TodoTable(props){
     return(
         <table className="table table-hover">
-                <thead>
-                <tr>
-                    <th scope='col'>#</th>
-                    <th scope='col'>Description</th>
-                    <th scope='col'>Assigned</th>
-                </tr>
-            </thead>
+             
             <tbody>
             <table className='table tbale-hover'>
         <thead>
@@ -20,18 +19,15 @@ function TodoTable(props){
         </thead>
         <tbody>
 
-        
-<TodoRowItem
-rowNumber ={props.todos[0].rowNumber} 
-rowDescription={props.todos[0].rowDescription} 
-rowAssigned = {props.todos[0].rowAssigned}
-/>
-<TodoRowItem
-rowNumber ={props.todos[1].rowNumber} rowDescription={props.todos[1].rowDescription} rowAssigned = {props.todos[1].rowAssigned}
-/>
-<TodoRowItem
-rowNumber ={props.todos[3].rowNumber} rowDescription={props.todos[3].rowDescription} rowAssigned = {props.todos[3].rowAssigned}
-/>
+        {props.todos.map(todo => (
+                    <TodoRowItem
+                        key={todo.rowNumber} 
+                        rowNumber={todo.rowNumber}
+                        rowDescription={todo.rowDescription}
+                        rowAssigned={todo.rowAssigned}
+                        deleteTodo={props.deleteTodo}
+                    />
+                ))}
         </tbody>
     </table>
                 
